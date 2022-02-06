de4e248fd96563d830e6e6d356863d86dd19ac9f
MrRicardoAcuna7 <mrricardoacuna@gmail.com>
2022-02-06 12:46:08 -0600

    Commit on main

--- Week1/docker_sql/pipeline.py
+++ Week1/docker_sql/pipeline.py
@@ -1,8 +1,9 @@
-import pandas as pd
 import sys
+
+import pandas as pd
 
 print(sys.argv)
 
 day = sys.argv[1]
 
-print(f'job finished succesfully for day = {day}')
+print(f'job finished successfully for day = {day}')
